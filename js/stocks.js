474a7eec19042a21c2fd6bd4d9e81bc3707570c8
sunnypatel2141 <sanipatel2141@hotmail.com>
2017-11-03 20:45:06 -0400

    Styling added to chart

--- js/stocks.js
+++ js/stocks.js
@@ -1,10 +1,12 @@
+// variables global
+var myMap = new Map();
+var api = "0XUH5ZVKMA8NIJ2J";
+var url, timeout = 120000;
+var chart;
+
 function chart() {
 
-	var symbol, url;
-	var api = "0XUH5ZVKMA8NIJ2J";
-
-	// Get the value of the input field with id="numb"
-	symbol = document.getElementById("numb").value;
+	var symbol = document.getElementById("numb").value;
 	
 	url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
 		+ symbol + "&interval=3600min&apikey=" + api; 
@@ -22,7 +24,6 @@ function chart() {
 	        var obj = JSON.parse(str);
 
 			var dailys = obj["Time Series (Daily)"];
-			var myMap = new Map();
 
 			for(var prop in dailys) {
 				var keyString = prop;
@@ -31,16 +32,20 @@ function chart() {
 				myMap.set(keyString, data);
 			}
 
-			var chart = new CanvasJS.Chart("chartContainer", {
+			chart = new CanvasJS.Chart("chartContainer", {
 				animationEnabled: true,
 				title:{
-					text: "Website Traffic"
+					text: "Stocks Quotes: " + symbol,
+					fontColor: "orange"
 				},
 				axisX:{
+					title : "Date",
+					titleFontColor: "black",
 					valueFormatString: "DD MMM"
 				},
 				axisY: {
 					title: "Stock Price",
+					titleFontColor: "black",
 					includeZero: false,
 					scaleBreaks: {
 						autoCalculate: true
@@ -67,28 +72,29 @@ function dataPoints(myMap) {
 	var i = 0, j = 10;
 	for (var [key, value] of myMap) {
 		var keyDate = key.split("-");
+		var month = parseInt(keyDate[1]) - 1;
 
 		//year-month-date and price (int)
-		array.push ({x : new Date(keyDate[0], keyDate[1], keyDate[2]),
+		array.push ({x : new Date(keyDate[0], month.toString(), keyDate[2]),
 					y : parseFloat(value)});
 		//console.log(keyDate[0] + value);
 	}
 	return array;
 }
 
-var t=setInterval(myFunction, 10000);
+// every 2 mins 
+var t=setInterval(myFunction, timeout);
 
 function myFunction() {
-	var symbol, url;
-	var api = "0XUH5ZVKMA8NIJ2J";
 
+	console.log("Fetching...");
+	
 	// Get the value of the input field with id="numb"
 	symbol = document.getElementById("numb").value;
+	
 	url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
 		+ symbol + "&interval=3600min&apikey=" + api; 
 
-	console.log(url);
-	
 	fetch(url, {method: 'get'
 	}).then(function(response) {
 		// Examine the text in the response
@@ -98,8 +104,12 @@ function myFunction() {
         var str = JSON.stringify(data);
         var obj = JSON.parse(str);
 
-         //"2017-11-03 13:07:00" or "2017-11-03"
-        var time = obj["Meta Data"]["3. Last Refreshed"].split(" ");
+        //"2017-11-03 13:07:00" or "2017-11-03"
+        if (hasWhiteSpace(obj["Meta Data"]["3. Last Refreshed"])) {
+        	var time = obj["Meta Data"]["3. Last Refreshed"].split(" ");	
+        } else {
+	        var time = obj["Meta Data"]["3. Last Refreshed"];
+        }
 
         /* {
 	        	"1. open": "1022.1100", 
@@ -108,48 +118,32 @@ function myFunction() {
 	        	"4. close": "1028.1900",
 	            "5. volume": "266535"
         	} */
-        var timeNowObj = obj["Time Series (Daily)"][time[0]];
+        var timeNowObj = obj["Time Series (Daily)"][time];
 
         //"1028.1900"
         var stockNow = timeNowObj["4. close"];
 
-		var dailys = obj["Time Series (Daily)"];
-		var timePrev;
-		var index = 0;
+        document.getElementById("demo").innerHTML = "The current stock price is " + 
+        	parseFloat(stockNow).toFixed(2);
 
-		var myMap = new Map();
+        //latest entry
+        chart.options.data[0].dataPoints[0].y = parseFloat(stockNow);
 
-		// get previous close
-		for(var prop in dailys) {
-			if (index == 1){
-				timePrev = dailys[prop];
-			}
-			index++;
 
-			document.getElementById("demo").innerHTML = "prop " + prop + " val " +
-			dailys[prop]["4. close"];
-
-			var keyString = prop;
-			var data = dailys[prop["4. close"]];
-
-			myMap.set(keyString, data);
-		}
-
-		var stockPrev = timePrev["4. close"];
-
-        // document.getElementById("demo").innerHTML = 
-        // "At " + time + ", the stock for " + symbol + " is "
-        // + stockNow + ". The previous stock close was " + stockPrev;
-
-        for (var [key, value] of myMap) {
-				//console.log(value);
-		}
+//		chart.render();
       });
 	}).catch(function(err) {
 		alert('Error occured. Restart the page.');
-	});	
+	});
+
+	console.log("Next fetch in " + Math.floor(timeout / 60000) + " minutes...");
+}
+
+function hasWhiteSpace(s) {
+  return s.indexOf(' ') >= 0;
 }
 
 function stopFunc() {
+	console.log("Stopped fetching...");
 	clearInterval(t);
 }	
